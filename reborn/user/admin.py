d0d1f00a5d4a7017eaf725f8beb1b77bfca0be0a
thoongee <94193480+thoongee@users.noreply.github.com>
2022-03-19 00:41:15 +0900

    Add files via upload

---
+++ reborn/user/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from user.models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email',) # 마지막에 , 안 넣으면 문자열로 인식해요.
+
+admin.site.register(User, UserAdmin)
